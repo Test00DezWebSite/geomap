dc5438fda350bf20dc0def35ce4bbffbaaf6f030
PkSM3 <pokesam3@gmail.com>
2013-11-25 09:28:44 -0300

    map colors+++++

--- php/mapael.php
+++ php/mapael.php
@@ -14,16 +14,23 @@ $norm_country=array();
 foreach ($base->query($sql) as $row) {
     $code=$country[$row["data"]];
     //$norm_country[$code] = array();//$row["count(*)"];
-    $info=array();
-    $info["value"]=$row["count(*)"];
-    $info["attrs"] = array();
-    $info["attrs"]["href"] = "#";
-    $info["tooltip"] = array();
-    
-    //$info["tooltip"]["content"] = "%3Cspan%20style%3D%22font-weight%3Abold%3B%22%3E".$code."%3C%2Fspan%3E%3Cbr%2F%3Enb_Publishers%20%3A".$row["count(*)"];
-    //$info["tooltip"]["content"] = "&lt;span&gt;".$code."&lt;&#47;span&gt;&lt;br;&lt;&gt;".$row["count(*)"];
-    $info["tooltip"]["content"] = "<span style='font-weight=bold;'>".$CC[$code]."</span><br/>Publications: ".$row["count(*)"];
-    $norm_country[$code]=$info;
+    $tempcount=0;
+    if($norm_country[$code]){
+        $norm_country[$code]["value"]+=$row["count(*)"];
+        $norm_country[$code]["tooltip"]["content"]="<span style='font-weight=bold;'>".$CC[$code]."</span><br/>Publications: ".$norm_country[$code]["value"];
+    }
+    else {        
+        $info=array();
+        $info["value"]=$row["count(*)"];
+        $info["attrs"] = array();
+        $info["attrs"]["href"] = "#";
+        $info["tooltip"] = array();
+
+        //$info["tooltip"]["content"] = "%3Cspan%20style%3D%22font-weight%3Abold%3B%22%3E".$code."%3C%2Fspan%3E%3Cbr%2F%3Enb_Publishers%20%3A".$row["count(*)"];
+        //$info["tooltip"]["content"] = "&lt;span&gt;".$code."&lt;&#47;span&gt;&lt;br;&lt;&gt;".$row["count(*)"];
+        $info["tooltip"]["content"] = "<span style='font-weight=bold;'>".$CC[$code]."</span><br/>Publications: ".$row["count(*)"];
+        $norm_country[$code]=$info;
+    }
 }
 
 echo json_encode($norm_country);
